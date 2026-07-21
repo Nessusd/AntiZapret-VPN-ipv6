8a4e944941c58a98d5a275a02631cce864115a8d
Nessusd <45721524+Nessusd@users.noreply.github.com>
2026-07-21 11:41:33 +0300

    Remove remaining OpenVPN IPv6 client block

--- setup/etc/openvpn/server/scripts/client-connect.sh
+++ setup/etc/openvpn/server/scripts/client-connect.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-if [[ "$IV_PLAT" != 'linux' ]]; then
-	echo 'push "ifconfig-ipv6 fdea:dead::2/64 fdea:dead::1"
-push "route-ipv6 ::/3"
-push "route-ipv6 2000::/4"
-push "route-ipv6 3000::/4"
-push "route-ipv6 fc00::/7"
-push "block-ipv6"' >> "$1"
-fi
-
+# IPv6 blocking was removed. Keep this hook for compatibility with
+# existing OpenVPN server configurations.
 exit 0
